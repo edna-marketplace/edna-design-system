9116b35880c13f9391bc924c598b5b8644351aba
matheusbarcc <matheusbarcon@gmail.com>
2025-04-24 15:55:43 -0300

    undo changes

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -33,10 +33,4 @@ export const {
     radii,
     space,
   },
-  media: {
-    sm: '(max-width: 640px)',
-    md: '(max-width: 768px)',
-    lg: '(max-width: 1024px)',
-    xl: '(max-width: 1500px)',
-  },
 })
