c5e2b1363974b8aa080859b7db55887d52af73cb
matheusbarcc <matheusbarcon@gmail.com>
2025-04-24 15:49:25 -0300

    chore: add media sizes

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -24,7 +24,6 @@ export const {
     height: 'space',
     width: 'space',
   },
-
   theme: {
     colors,
     fontSizes,
@@ -33,5 +32,11 @@ export const {
     lineHeights,
     radii,
     space,
+  },
+  media: {
+    sm: '(max-width: 640px)',
+    md: '(max-width: 768px)',
+    lg: '(max-width: 1024px)',
+    xl: '(max-width: 1500px)',
   },
 })
